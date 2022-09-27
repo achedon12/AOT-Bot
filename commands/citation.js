3439ebd420d289bd357b72ef54a5dee50359aa0c
Achedon12 <leoderoinleo@gmail.com>
2022-09-27 09:56:01 +0200

    patch citation command

--- commands/citation.js
+++ commands/citation.js
@@ -1,5 +1,6 @@
 const Index = require("../index");
 const fs = require("fs");
+const {EmbedBuilder} = require("discord.js");
 
 module.exports.config = {
     name: "citation",
@@ -15,30 +16,65 @@ module.exports.network = {
             name: "nom",
             description: "nom du prof",
             type: "STRING",
-            required: true
+            required: false
         }
     ]
 }
 
-
-
 module.exports.execute = function (member, channel, guild, args, Client, interaction) {
 
     const cii = JSON.parse(fs.readFileSync(`${Index.dirname}/citations.json`));
+    let citations = [];
+    if(args[0] === undefined){
+        let url;
+        let name;
+        let object = cii[Math.floor(Math.random() * cii.length)];
 
-    if(Index.profs.includes(args[0].toLowerCase())){
-        let citations;
-        for (let i = 0; i < cii.length; i++) {
-            if(cii[i].name === args[0]){
-                citations = cii[i].citations;
-            }
+        for (let j = 0; j < object.citations.length; j++) {
+            citations.push(object.citations[j])
+            url = object.url;
+            name = object.name;
         }
-        if(citations.length !== 0){
-            interaction.reply(citations[Math.floor(Math.random() * citations.length)]);
-        }else{
-            interaction.reply("⛔ Aucune citations pour ce prof !");
-        }
+
+        interaction.reply(`${citations.length} citations de ${name}`)
+        let rand = Math.floor(Math.random() * citations.length);
+        const embed = new EmbedBuilder()
+            .setColor("#f825fc")
+            .setTitle(`${citations[rand].text} - ${citations[rand].date}`)
+            .setImage(url)
+            .setDescription("Citation de :")
+        channel.send({embeds: [embed]});
     }else{
-        interaction.reply("⛔ Ce prof est invalide !");
+        if(Index.profs.includes(args[0].toLowerCase())){
+            let url;
+            let object;
+            for (let i = 0; i < cii.length; i++) {
+                if(cii[i].name === args[0]){
+                    object = cii[i];
+                }
+            }
+
+            for (let j = 0; j < object.citations.length; j++) {
+                citations.push(object.citations[j])
+                url = object.url;
+
+            }
+
+            if(citations.length !== 0){
+                interaction.reply(`${citations.length} citations de ${args[0]}`)
+                let rand = Math.floor(Math.random() * citations.length);
+                const embed = new EmbedBuilder()
+                    .setColor("#f825fc")
+                    .setTitle(`${citations[rand].text} - ${citations[rand].date}`)
+                    .setImage(url)
+                    .setDescription("Citation de :")
+                channel.send({embeds: [embed]});
+            }else{
+                interaction.reply("⛔ Aucune citations pour cette personne !");
+            }
+        }else{
+            interaction.reply("⛔ Ce prof est invalide !");
+        }
     }
+
 }
