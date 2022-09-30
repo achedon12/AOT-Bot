9d24bf92045c00a5672a87e0751c3dfe7a8cbecc
Achedon12 <leoderoinleo@gmail.com>
2022-09-30 10:27:10 +0200

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -27,23 +27,27 @@ module.exports.execute = async function (member, channel, guild, args, Client, i
     if(args[0] === undefined){
         blague = await blagues.randomCategorized(blagues.categories.GLOBAL);
     }else{
-
-        switch (args[0]){
-            case "global":
-                blague = await blagues.randomCategorized(blagues.categories.GLOBAL);
-                break;
-            case "dev":
-                blague = await blagues.randomCategorized(blagues.categories.DEV);
-                break;
-            case "dark":
-                blague = await blagues.randomCategorized(blagues.categories.DARK);
-                break;
-            case "beauf":
-                blague = await blagues.randomCategorized(blagues.categories.BEAUF);
-                break;
-            case "blondes":
-                blague = await blagues.randomCategorized(blagues.categories.BLONDES);
-                break;
+        let list = ["global","dev","dark","beauf","blondes"];
+        if(list.includes(args[0])){
+            switch (args[0]){
+                case "global":
+                    blague = await blagues.randomCategorized(blagues.categories.GLOBAL);
+                    break;
+                case "dev":
+                    blague = await blagues.randomCategorized(blagues.categories.DEV);
+                    break;
+                case "dark":
+                    blague = await blagues.randomCategorized(blagues.categories.DARK);
+                    break;
+                case "beauf":
+                    blague = await blagues.randomCategorized(blagues.categories.BEAUF);
+                    break;
+                case "blondes":
+                    blague = await blagues.randomCategorized(blagues.categories.BLONDES);
+                    break;
+            }
+        }else{
+            blague = await blagues.randomCategorized(blagues.categories.GLOBAL);
         }
     }
     interaction.reply(blague.joke);
